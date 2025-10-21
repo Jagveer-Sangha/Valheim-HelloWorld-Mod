36a1df77e3ada62232d16263c9d3bcff0553ab29
Jagveer-Sangha <jagveersangha@gmail.com>
2025-10-21 02:05:32 -0400

    Add project files.

---
+++ ValheimHelloWorld/HelloWorldPlugin.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using UnityEngine;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+
+//[BepInPlugin("com.yourname.helloworld", "HelloWorld", "1.0.0")]
+public class HelloWorldPlugin : BaseUnityPlugin
+{
+    private void Start()
+    {
+        Logger.LogInfo("Hello, Valheim modding world!");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F5))
+        {
+            Logger.LogInfo("You pressed F5!");
+        }
+    }
+
+    private void OnDestroy()
+    {
+        Logger.LogInfo("Goodbye, Valheim modding world!");
+    }
+}
